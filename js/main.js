2bfdee65eb91b077fce913ea41347f92e946dc6b
Karina Zhuro <kzhuro36@gmail.com>
2020-05-27 10:13:43 +0300

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,14 @@
+const canvas = document.getElementById("canvas");
+const ctx = canvas.getContext("2d");
+
+ctx.beginPath();
+ctx.rect(20, 40, 50, 50);
+ctx.fillStyle = "#FF0000";
+ctx.fill();
+ctx.closePath();
+
+ctx.beginPath();
+ctx.arc(240, 160, 20, 0, Math.PI*2, false);
+ctx.fillStyle = "green";
+ctx.fill();
+ctx.closePath();
